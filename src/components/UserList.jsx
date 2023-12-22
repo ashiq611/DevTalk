6a2d81c8f5f72bebaa8e9668e05de8193bc6ba12
ashiq611 <72414858+ashiq611@users.noreply.github.com>
2023-12-22 13:08:36 +0600

    following and followers features done

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,53 +1,97 @@
 
 import  { useEffect, useState } from 'react';
 import { fireDB } from '../firebase.confiq';
-import { onValue, ref } from 'firebase/database';
+import { onValue, push, ref, remove, set } from 'firebase/database';
+import { useSelector } from 'react-redux';
 
 const UserList = () => {
-    const [userList, setUserList] = useState([])
+  const [userList, setUserList] = useState([]);
+  const [following, setFollowing] = useState([]);
+  const data = useSelector((state) => state.userLoginInfo.userInfo);
 
-     useEffect(() => {
-       const blogRef = ref(fireDB, "users");
-       onValue(blogRef, (snapshot) => {
-         let users = [];
-         snapshot.forEach((b) => {
-           users.push({ ...b.val(), id: b.key });
-         });
-         setUserList(users);
-       });
-     }, []);
+  useEffect(() => {
+    const blogRef = ref(fireDB, "users");
+    onValue(blogRef, (snapshot) => {
+      let users = [];
+      snapshot.forEach((b) => {
+        if (data.uid != b.key) {
+          users.push({ ...b.val(), id: b.key });
+        }
+      });
+      setUserList(users);
+    });
+  }, [data.uid]);
 
-    return (
-        <div>
-            {
-                userList.map((user) => {
-                    return (
-                      <div
-                        key={user.id}
-                        className="max-w-md mx-auto my-5 bg-indigo-500 text-center rounded-lg shadow-md overflow-hidden"
-                      >
-                        <div className="p-4">
-                          <img
-                            src={user.profile_picture} // Replace with your profile photo URL
-                            alt="Profile"
-                            className="w-24 h-24 rounded-full mx-auto mb-4"
-                          />
-                          <h2 className="text-xl font-semibold text-white text-center mb-2">
-                            {user.username}
-                          </h2>
-                          <button
-                            to="/createblog"
-                            className="bg-blue-500 text-white font-bold px-4 py-2 rounded-full mx-auto"
-                          >
-                            Follow
-                          </button>
-                        </div>
-                      </div>
-                    );
-                })
-            }
-        </div>
-    );
+  useEffect(() => {
+    const followRef = ref(fireDB, "follow");
+    onValue(followRef, (snapshot) => {
+      let users = [];
+      snapshot.forEach((f) => {
+        if (data.uid != f.val().senderID) {
+          users.push({ ...f.val(), id: f.key });
+        }
+      });
+      setFollowing(users);
+    });
+  }, [data.uid]);
+
+  // send follow starts
+  const handleFollow = (user) => {
+    console.log(user);
+    set(push(ref(fireDB, "follow")), {
+      senderID: data.uid,
+      senderName: data.displayName,
+      senderProfile: data.photoURL,
+      receiverID: user.id,
+      receiverName: user.username,
+      receiverProfile: user.profile_picture,
+    });
+  };
+
+  // // cancel req
+  // const handleUnfollow = (user) => {
+  //   remove(ref(fireDB, "follow/" + user.id));
+  // };
+
+  return (
+    <div>
+      {userList.map((user) => {
+        return (
+          <div
+            key={user.id}
+            className="max-w-md mx-auto my-5 bg-indigo-500 text-center rounded-lg shadow-md overflow-hidden"
+          >
+            <div className="p-4">
+              <img
+                src={user.profile_picture} // Replace with your profile photo URL
+                alt="Profile"
+                className="w-24 h-24 rounded-full mx-auto mb-4"
+              />
+              <h2 className="text-xl font-semibold text-white text-center mb-2">
+                {user.username}
+              </h2>
+              {following ? (
+                
+                <button
+                  // onClick={() => handleUnfollow(user)}
+                  className="bg-blue-500 text-white font-bold px-4 py-2 rounded-full mx-auto"
+                >
+                  Following
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleFollow(user)}
+                  className="bg-blue-500 text-white font-bold px-4 py-2 rounded-full mx-auto"
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default UserList;
--- src/pages/admin/Profile.jsx
+++ src/pages/admin/Profile.jsx
@@ -9,14 +9,16 @@ import Nav from "../../components/Nav";
 
 const Profile = () => {
   const [Allblogs, setAllBlogs] = useState([]);
+  const [following, setFollowing] = useState([]);
+  const [follower, setFollower] = useState([]);
   const user = useSelector((state) => state.userLoginInfo.userInfo);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   useEffect(() => {
-     if (!user) {
-       navigate("/");
-     }
-   });
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  });
 
   useEffect(() => {
     const blogRef = ref(fireDB, "blogs");
@@ -31,6 +33,36 @@ const Profile = () => {
     });
   }, [user.uid]); // Empty dependency array to fetch blogs only once when the component mounts
 
+  // following data
+  useEffect(() => {
+    const followRef = ref(fireDB, "follow");
+    onValue(followRef, (snapshot) => {
+      let follows = [];
+      snapshot.forEach((f) => {
+        if (f.val().senderID == user.uid) {
+          follows.push({ ...f.val(), id: f.key });
+        }
+      });
+      setFollowing(follows);
+    });
+  }, [user.uid]);
+
+  // follower data
+  useEffect(() => {
+    const followRef = ref(fireDB, "follow");
+    onValue(followRef, (snapshot) => {
+      let follows = [];
+      snapshot.forEach((f) => {
+        if (f.val().receiverID == user.uid) {
+          follows.push({ ...f.val(), id: f.key });
+        }
+      });
+      setFollower(follows);
+    });
+  }, [user.uid]);
+
+  console.log(follower);
+
   // cancel req
   const handleBlogRemove = (b) => {
     remove(ref(fireDB, "blogs/" + b.id));
@@ -40,7 +72,7 @@ const Profile = () => {
 
   return (
     <div>
-      <Nav/>
+      <Nav />
       <div>
         <div className="container mx-auto p-8">
           <div className="max-w-md mx-auto bg-indigo-500 text-center rounded-lg shadow-md overflow-hidden">
@@ -53,12 +85,97 @@ const Profile = () => {
               <h2 className="text-xl font-semibold text-white text-center mb-2">
                 {user.displayName}
               </h2>
-              <Link to='/createblog' className="bg-blue-500 text-white px-4 py-2 rounded-full mx-auto">
+              <Link
+                to="/createblog"
+                className="bg-blue-500 text-white px-4 py-2 rounded-full mx-auto"
+              >
                 Create Blog
               </Link>
+              <div>
+                <button
+                  onClick={() =>
+                    document.getElementById("my_modal_5").showModal()
+                  }
+                >
+                  Following
+                </button>
+                <button
+                  onClick={() =>
+                    document.getElementById("my_modal_6").showModal()
+                  }
+                >
+                  Followers
+                </button>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="following">
+        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <div>
+              {following.map((f) => (
+                <div
+                  key={f.id}
+                  className="max-w-md mx-auto bg-indigo-500 text-center rounded-lg shadow-md overflow-hidden"
+                >
+                  <div className="p-4">
+                    <img
+                      src={f.receiverProfile} // Replace with your profile photo URL
+                      alt="Profile"
+                      className="w-24 h-24 rounded-full mx-auto mb-4"
+                    />
+                    <h2 className="text-xl font-semibold text-white text-center mb-2">
+                      {f.receiverName}
+                    </h2>
+
+                    <div>
+                      <button>Unfollow</button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+      <div className="follower">
+        <dialog id="my_modal_6" className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <div>
+              {follower.map((f) => (
+                <div
+                  key={f.id}
+                  className="max-w-md mx-auto bg-indigo-500 text-center rounded-lg shadow-md overflow-hidden"
+                >
+                  <div className="p-4">
+                    <img
+                      src={f.senderProfile} // Replace with your profile photo URL
+                      alt="Profile"
+                      className="w-24 h-24 rounded-full mx-auto mb-4"
+                    />
+                    <h2 className="text-xl font-semibold text-white text-center mb-2">
+                      {f.senderName}
+                    </h2>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
       </div>
       <div className="w-full p-4">
         <div className="text-3xl font-semibold mb-4">Blog List</div>
